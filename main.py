94ce7c5b71b01992f441588371430dca7265b020
alemagno10 <alemagno03@gmail.com>
2024-04-20 20:17:14 -0300

    feat: movimentacao routes

--- main.py
+++ main.py
@@ -24,6 +24,43 @@ movimentacoes = [
 def root():
     return {"Hello": "World"}
 
+@app.get("/movimentacao", response_model=List[Movimentacao])
+def get_movimentacao():
+    return movimentacoes
 
+@app.post("/movimentacao", response_model=Movimentacao, status_code=status.HTTP_201_CREATED)
+def nova_movimentacao(mov : Movimentacao):
+    movimentacoes.append(mov)
+    return mov
+
+@app.get("/movimentacao/{id_encomenda}", response_model=List[Movimentacao])
+def get_movimentacao(id_encomenda: int):
+    movimentacoesDaEncomenda = list(filter(lambda mov: mov.id_encomenda == id_encomenda, movimentacoes))
+    if len(movimentacoesDaEncomenda) > 0:
+        return movimentacoesDaEncomenda
+    raise HTTPException(status_code=404, detail="Nenhuma movimentação encontrada para a encomenda informada")
+
+@app.get("/movimentacao/{id_encomenda}/{id_movimentacao}", response_model=Movimentacao)
+def get_movimentacao(id_encomenda: int, id_movimentacao: int):
+    movimentacao = next(filter(lambda mov: mov.id_encomenda == id_encomenda and mov.id_movimentacao == id_movimentacao, movimentacoes), None)
+    if movimentacao:
+        return movimentacao
+    raise HTTPException(status_code=404, detail="Movimentação não encontrada")
+
+@app.put("/movimentacao/{id_movimentacao}", response_model=Movimentacao)
+def atualiza_movimentacao(id_movimentacao: int, mov: Movimentacao):
+    mov = next(filter(lambda mov: mov.id_movimentacao == id_movimentacao, movimentacoes), None)
+    if mov:
+        mov.status = mov.status
+        return mov
+    raise HTTPException(status_code=404, detail="Movimentação não encontrada")
+
+@app.delete("/movimentacao/{id_movimentacao}", status_code=status.HTTP_204_NO_CONTENT)
+def deleta_movimentacao(id_movimentacao: int):
+    mov = next(filter(lambda mov: mov.id_movimentacao == id_movimentacao, movimentacoes), None)
+    if mov:
+        movimentacoes.remove(mov)
+        return {"message": "Movimentação removida com sucesso"}
+    raise HTTPException(status_code=404, detail="Movimentação não encontrada")
 
 
