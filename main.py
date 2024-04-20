45e2f921910b478b1fd42d9c11646dd0f9c7fc2f
alemagno10 <alemagno03@gmail.com>
2024-04-20 20:16:57 -0300

    feat: movimentacao model

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 # run: uvicorn main:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status # type: ignore
+from typing import List
+import datetime
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Movimentacao(BaseModel):
+    id_movimentacao: int
+    id_encomenda: int
+    id_localizacao: int
+    status: str
+    data: datetime.datetime
+
+movimentacoes = [
+    Movimentacao(id_movimentacao=1, id_encomenda=1, id_localizacao=1, status="Em trânsito", data=datetime.datetime.now()),
+    Movimentacao(id_movimentacao=2, id_encomenda=1, id_localizacao=2, status="Em trânsito", data=datetime.datetime.now())
+]
+
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+
+
+
